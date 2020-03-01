3506bd1b575f3620b7f5ceb3ffbbe9d1dd426a39
Jared Jessup <jessupjs@gmail.com>
2020-03-01 12:22:19 -0500

    Sort

--- client/bookitup/js/main.js
+++ client/bookitup/js/main.js
@@ -1,16 +1,28 @@
 'use strict';
 
+// radials
+let radial1, radial2, radial3;
+
 // Test 1 : randoms
 d3.json(`data/scrapy-randoms_set1_100.json`).then(d => {
-    const radial = new Radial(d, 'radial1')
+    radial1 = new Radial(d, 'radial1')
 }).catch(err => console.log(err));
 
 // Test 2 : similars
 d3.json(`data/scrapy-similars_40961621_100.json`).then(d => {
-    const radial = new Radial(d, 'radial2')
+    radial2 = new Radial(d, 'radial2')
 }).catch(err => console.log(err));
 
 // Test 3 : similars
 d3.json(`data/scrapy-TamaraMunzner.json`).then(d => {
-    const radial = new Radial(d, 'radial3')
+    radial3 = new Radial(d, 'radial3')
 }).catch(err => console.log(err));
+
+/*
+ sortIt
+ */
+function sortIt(passthru) {
+    radial1.sortIt(passthru.value)
+    radial2.sortIt(passthru.value)
+    radial3.sortIt(passthru.value)
+}
--- client/bookitup/js/radial.js
+++ client/bookitup/js/radial.js
@@ -85,6 +85,9 @@ class Radial {
         // Config
         vis.dotG = 3;
 
+        // Init
+        vis.sort = 'abc';
+
         // :^)
         vis.wrangleData();
     }
@@ -105,6 +108,13 @@ class Radial {
             .rollup(v => v.length)
             .entries(genre);
 
+        // Sort
+        if (vis.sort === 'abc') {
+            vis.genreData.sort((a, b) => b.key - a.key);
+        } else {
+            vis.genreData.sort((a, b) => b.value - a.value);
+        }
+
         // Update scale (pre)
         vis.coordScale.domain([0, vis.genreData.length]);
         vis.yScale.domain([0, d3.max(vis.genreData, d => d.value)]);
@@ -122,11 +132,11 @@ class Radial {
 
         // Filter out entries with no genre and reduce genres to sets
         vis.bookData = vis.data.filter(d => d.hasOwnProperty('genres') && d.genres.length !== 0);
-        /*vis.bookData.forEach(d => {
+        vis.bookData.forEach(d => {
             if (d.hasOwnProperty('genres')) {
                 d.genres = new Set(d.genres);
             }
-        });*/
+        });
 
         // Init geoMaker
         vis.geoMaker = d3.line()
@@ -148,7 +158,11 @@ class Radial {
                 enter => enter
                     .append('path')
                     .attr('class', 'areaPath')
-                    .attr('d', d => vis.areaMaker(d.area))
+                    .attr('d', d => vis.areaMaker(d.area)),
+                update => update
+                    .transition()
+                    .attr('d', d => vis.areaMaker(d.area)),
+                exit => exit.remove()
             );
 
         // Update genre count text
@@ -216,27 +230,87 @@ class Radial {
                                 .attr('cy', yAve);
                         }
 
-                    }).on('mouseover', function (d) {
-                        // Clear selections
-                        vis.g.select('.geoPathSel')
-                            .classed('geoPathSel', false);
-                        vis.g.select('.geoCircSel')
-                            .classed('geoCircSel', false);
-                        vis.g.selectAll('.geoNodeSel')
-                            .classed('geoNodeSel', false);
-                        // Define this
-                        const g = d3.select(this);
-                        // Show path and outline circ
-                        g.select('.geoPath')
-                            .classed('geoPathSel', true);
-                        g.select('.geoCirc')
-                            .classed('geoCircSel', true);
-                        g.selectAll('.geoNode')
-                            .classed('geoNodeSel', true);
+                    }),
+                // UPDATE
+                update => update
+                    .each(function (d) {
+                        // Get this geoG
+                        const geoG = d3.select(this);
+                        // Collect genres
+                        const genres = [];
+                        d.genres.forEach(g => {
+                            const currentGenre = vis.genreData.find(cg => {
+                                if (cg.hasOwnProperty('key')) {
+                                    return cg.key === g;
+                                }
+                            });
+                            genres.push(currentGenre);
+                        });
+                        // Sort by index
+                        genres.sort((a, b) => a.index - b.index);
+                        // Add to coords
+                        const coords = [];
+                        genres.forEach(g => {
+                            coords.push([g.x, g.y]);
+                        });
+                        // Draw path
+                        geoG.select('.geoPath')
+                            .attr('d', vis.geoMaker(coords));
+                        // Draw node circles
+                        genres.forEach(g => {
+                            geoG.select('.geoNode')
+                                .attr('cx', g.x)
+                                .attr('cy', g.y)
+                        });
+                        // Get center of polygon
+                        const centroid = d3.polygonCentroid(coords);
+                        if (centroid[0] && centroid[1]) {
+                            // Draw circ
+                            geoG.select('.geoCirc')
+                                .transition()
+                                .attr('cx', centroid[0])
+                                .attr('cy', centroid[1]);
+                        } else {
+                            // Calc circ ave coords
+                            const xAve = coords.map(d => d[0]).reduce((a, c) => a + c) / coords.length;
+                            const yAve = coords.map(d => d[1]).reduce((a, c) => a + c) / coords.length;
+                            // Draw circ
+                            geoG.select('.geoCirc')
+                                .transition()
+                                .attr('cx', xAve)
+                                .attr('cy', yAve);
+                        }
+
                     })
-            )
+            ).on('mouseover', function (d) {
+            // Clear selections
+            vis.g.select('.geoPathSel')
+                .classed('geoPathSel', false);
+            vis.g.select('.geoCircSel')
+                .classed('geoCircSel', false);
+            vis.g.selectAll('.geoNodeSel')
+                .classed('geoNodeSel', false);
+            // Define this
+            const g = d3.select(this);
+            // Show path and outline circ
+            g.select('.geoPath')
+                .classed('geoPathSel', true);
+            g.select('.geoCirc')
+                .classed('geoCircSel', true);
+            g.selectAll('.geoNode')
+                .classed('geoNodeSel', true);
+        })
+    }
 
+    sortIt(by) {
+        // Get this vis
+        const vis = this;
 
+        // Update sort
+        vis.sort = by;
+
+        // Wrangle
+        vis.wrangleData();
     }
 
 
