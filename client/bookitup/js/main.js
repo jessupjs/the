9cd76b1d30611abede3c881f5364e5814dd3f6b6
Jared Jessup <jessupjs@gmail.com>
2020-03-06 09:32:15 -0500

    New users

--- client/bookitup/js/main.js
+++ client/bookitup/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // radials
-let orderedRadialZonaK;
-let radialZonaK;
-let roseZonaK;
+let orderedRadialZonaK, orderedRadialJeffB, orderedRadialJaredJ;
+let radialZonaK, radialJeffB, radialJaredJ;
+let roseZonaK, roseJeffB, roseJaredJ;
 
 let _hierarchy;
 
@@ -15,7 +15,7 @@ d3.json('data/genre_hierarchy.json').then(d => {
     console.log('////////////////////////// GENRE HIERARCHY');
     console.log(_hierarchy);
 
-    // Test 3B : custom - Zona Kostic
+    // Test : Zona Kostic
     d3.json(`data/scrapy-ZonaKostic.json`).then(d => {
         const data1 = d;
         const data2 = JSON.parse(JSON.stringify(d));
@@ -24,12 +24,34 @@ d3.json('data/genre_hierarchy.json').then(d => {
         roseZonaK = new Rose(data2, 'roseZonaK');
     }).catch(err => console.log(err));
 
+    // Test : Jeff Baglioni
+    d3.json(`data/scrapy-JeffBaglioni.json`).then(d => {
+        const data1 = d;
+        const data2 = JSON.parse(JSON.stringify(d));
+        orderedRadialJeffB = new OrderedRadial(data1, 'orderedRadialJeffB');
+        radialJeffB = new Radial(data1, 'radialJeffB');
+        roseJeffB = new Rose(data2, 'roseJeffB');
+    }).catch(err => console.log(err));
+
+    // Test : Jared Jessup
+    d3.json(`data/scrapy-JaredJessup.json`).then(d => {
+        const data1 = d;
+        const data2 = JSON.parse(JSON.stringify(d));
+        orderedRadialJaredJ = new OrderedRadial(data1, 'orderedRadialJaredJ');
+        radialJaredJ = new Radial(data1, 'radialJaredJ');
+        roseJaredJ = new Rose(data2, 'roseJaredJ');
+    }).catch(err => console.log(err));
+
 }).catch(err => console.log(err));
 
 /*
  sortIt
  */
 function sortIt(passthru) {
-    radial1.sortIt(passthru.value);
-    rose1.sortIt(passthru.value);
+    radialZonaK.sortIt(passthru.value);
+    roseZonaK.sortIt(passthru.value);
+    radialJeffB.sortIt(passthru.value);
+    roseJeffB.sortIt(passthru.value);
+    radialJaredJ.sortIt(passthru.value);
+    roseJaredJ.sortIt(passthru.value);
 }
