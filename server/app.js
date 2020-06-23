69963be87e4e428dfd15634bcc13025945c42892
Jared Jessup <jessupjs@gmail.com>
2020-06-23 11:02:30 -0400

    Lensing demo

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var leapmotionRouter = require('./routes/api/leapmotion');
-var vcgRouter = require('./routes/api/vcg');
 
 var app = express();
 
@@ -28,10 +27,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/leapmotion', leapmotionRouter);
 app.use('/leaptutorial', express.static(path.join(__dirname, '../client/leaptutorial')));
+app.use('/vcg/lensing-demo', express.static(path.join(__dirname, '../client/vcg/lensing-demo/dist')));
 app.use('/vcg/nodes', express.static(path.join(__dirname, '../client/vcg/nodes')));
 app.use('/vcg/table2text', express.static(path.join(__dirname, '../client/vcg/table2text')));
-app.use('/bookvis/demo', express.static(path.join(__dirname, '../client/bookvis/demo')));
-//app.use('/vcg', vcgRouter); TODO
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
